0c449af3af8d2903bea7120f440733d106b5312c
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-04-09 15:25:38 -0700

    wrote failing test for get all.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const db = require('../lib/utils/database');
+const Studio = require('../lib/models/Studio');
 
 describe('lab-11-ripe-banana routes', () => {
   beforeEach(() => {
     return db.sync({ force: true });
+  });
+  let Studios;
+  beforeEach(() => {
+    Studios = Studio.create({
+      name: 'Laika',
+      city: 'Hillsboro',
+      state: 'OR',
+      country: 'US of A',
+    });
   });
 
   it('it posts to the studio db', () => {
@@ -19,12 +29,28 @@ describe('lab-11-ripe-banana routes', () => {
       })
       .then((res) => {
         expect(res.body).toEqual({
-          id: 1,
+          id: 2,
           name: 'Laika',
           city: 'Hillsboro',
           state: 'OR',
           country: 'US of A',
         });
+      });
+  });
+
+  it('returns all studios from db', () => {
+    return request(app)
+      .get('/api/v1/studios')
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            id: 1,
+            name: 'Laika',
+            city: 'Hillsboro',
+            state: 'OR',
+            country: 'US of A',
+          },
+        ]);
       });
   });
 });
