e558079544eedfb65bb311d7e6e89d80ab01b89c
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-04-09 12:00:40 -0700

    added first test for studio routes.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,30 @@
-const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const db = require('../lib/utils/database');
 
 describe('lab-11-ripe-banana routes', () => {
   beforeEach(() => {
-    return setup(pool);
+    return db.sync({ force: true });
+  });
+
+  it('it posts to the studio db', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'Laika',
+        city: 'Hillsboro',
+        state: 'OR',
+        country: 'US of A',
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'Laika',
+          city: 'Hillsboro',
+          state: 'OR',
+          country: 'US of A',
+        });
+      });
   });
 });
