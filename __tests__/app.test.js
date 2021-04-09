6f83d29431a6a2156f0ef8bab17cb893f1080402
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-04-09 15:51:12 -0700

    wrote first test for actor db.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,7 @@ const app = require('../lib/app');
 const db = require('../lib/utils/database');
 const Studio = require('../lib/models/Studio');
 
-describe('lab-11-ripe-banana routes', () => {
+describe('Studio routes', () => {
   beforeEach(() => {
     return db.sync({ force: true });
   });
@@ -64,6 +64,30 @@ describe('lab-11-ripe-banana routes', () => {
           city: 'Hillsboro',
           state: 'OR',
           country: 'US of A',
+        });
+      });
+  });
+});
+
+describe('Actor routes', () => {
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+
+  it('posts an actor to the actor db', () => {
+    return request(app)
+      .post('/api/v1/actors')
+      .send({
+        name: 'Val Kilmer',
+        dob: '31/12/1959',
+        pob: 'Los Angeles, CA',
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'Val Kilmer',
+          dob: '31/12/1959',
+          pob: 'Los Angeles, CA',
         });
       });
   });
